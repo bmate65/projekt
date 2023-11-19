9571f982023feacb6e95cec52b5d3485f1b6a579
KSandor2003 <147424430+KSandor2003@users.noreply.github.com>
2023-11-19 14:42:27 +0100

    API ninja lekérés és befejezetlen számítás

--- ProjektLabor/astronomy.js
+++ ProjektLabor/astronomy.js
@@ -12,14 +12,28 @@
         var nameValue = document.getElementById("astronomy").value;
         const t = new Date();
         var timeC = t.toLocaleTimeString();
-        timeC.replace(":","%3A");
+        timeC = timeC.replaceAll(":","%3A");
         let d = new Date();
         let dateC = d.toISOString().split('T')[0];
         let latitudeC = document.getElementById("latitude").value;
         let longitudeC = document.getElementById("longitude").value;
         let elevationC = document.getElementById("elevation").value;
 
-        fetch('https://api.astronomyapi.com/api/v2/bodies/positions/'+nameValue+'?longitude='+longitudeC+'&latitude='+latitudeC+'&elevation='+elevationC+'&from_date='+dateC+'&to_date='+dateC+'&time='+timeC, {
+        $.ajax({
+            method: 'GET',
+            url: 'https://api.astronomyapi.com/api/v2/bodies/positions/'+nameValue+'?longitude='+longitudeC+'&latitude='+latitudeC+'&elevation='+elevationC+'&from_date='+dateC+'&to_date='+dateC+'&time='+timeC,
+            headers: { 'Authorization': `Basic ${authString}`},
+            contentType: 'application/json',
+            success: function(result) {
+                object = result;
+                display();
+                lathatoe();
+            },
+            error: function ajaxError(jqXHR) {
+                console.error('Error: ', jqXHR.responseText);
+            }
+        });
+        /*fetch('https://api.astronomyapi.com/api/v2/bodies/positions/'+nameValue+'?longitude='+longitudeC+'&latitude='+latitudeC+'&elevation='+elevationC+'&from_date='+dateC+'&to_date='+dateC+'&time='+timeC, {
             method: 'GET',
             headers: {
                 'Authorization': `Basic ${authString}`
@@ -34,7 +48,7 @@
         })
         .catch(error => {
             responseText.textContent = 'Hiba: ' + error.message;
-        });
+        });*/
         }else{
             var name = document.getElementById('Csillagok').value;
             $.ajax({
@@ -43,7 +57,8 @@
                 headers: { 'X-Api-Key': 'vHLde9Uzlap8vNokJq92rw==SZZqdSFE50w3UUI1'},
                 contentType: 'application/json',
                 success: function(result) {
-                    console.log(result);
+                    object = result;
+                    display();
                 },
                 error: function ajaxError(jqXHR) {
                     console.error('Error: ', jqXHR.responseText);
@@ -55,50 +70,78 @@
 
     function display() {
         clear();
-        var drawAzimuth = object.data.table.rows[0].cells[0].position.horizontal.azimuth.degrees;
-        drawAzimuth-=90;
-        document.getElementById('response').innerHTML = "";
-        var c = document.getElementById('Astronomy1');
-        var ctx = c.getContext("2d");
-        x1 = 200;
-        y1 = 200;
-        r =  150;
-        ctx.beginPath();
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x1 + r * Math.cos(Math.PI *drawAzimuth / 180.0), y1 + r * Math.sin(Math.PI * drawAzimuth / 180.0));
-        ctx.stroke();
-        ctx.font = "30px Arial";
-        ctx.fillText("N", 190, 35);
-        ctx.fillText("S", 190, 390);
-        ctx.fillText("W", 10, 205);
-        ctx.fillText("E", 360, 205);
+        if(document.getElementById("astronomy").value != "Star"){
+            var drawAzimuth = object.data.table.rows[0].cells[0].position.horizontal.azimuth.degrees;
+            drawAzimuth-=90;
+            document.getElementById('response').innerHTML = "";
+            var c = document.getElementById('Astronomy1');
+            var ctx = c.getContext("2d");
+            x1 = 200;
+            y1 = 200;
+            r =  150;
+            ctx.beginPath();
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x1 + r * Math.cos(Math.PI *drawAzimuth / 180.0), y1 + r * Math.sin(Math.PI * drawAzimuth / 180.0));
+            ctx.stroke();
+            ctx.font = "30px Arial";
+            ctx.fillText("N", 190, 35);
+            ctx.fillText("S", 190, 390);
+            ctx.fillText("W", 10, 205);
+            ctx.fillText("E", 360, 205);
 
-        var drawAltitude = object.data.table.rows[0].cells[0].position.horizontal.altitude.degrees;
-        var c = document.getElementById('Astronomy2');
-        var ctx = c.getContext("2d");
-        x1 = 20;
-        y1 = 360;
-        r = 350;
-        x2 = x1 + r * Math.cos(Math.PI * -drawAltitude / 180.0);
-        y2 = y1 + r * Math.sin(Math.PI * -drawAltitude / 180.0);
-        ctx.beginPath();
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(390, 360);
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
-        ctx.moveTo(x2, y2);
-        
-        ctx.arc(x2, y2, 10, 0, 2 * Math.PI);
-        ctx.fillStyle = "#FF0000";
-        ctx.moveTo(x1,y1);
-        ctx.arc(x1, y1, 10, 0, 2 * Math.PI);
-        ctx.fill();
+            var drawAltitude = object.data.table.rows[0].cells[0].position.horizontal.altitude.degrees;
+            var c = document.getElementById('Astronomy2');
+            var ctx = c.getContext("2d");
+            x1 = 20;
+            y1 = 360;
+            r = 350;
+            x2 = x1 + r * Math.cos(Math.PI * -drawAltitude / 180.0);
+            y2 = y1 + r * Math.sin(Math.PI * -drawAltitude / 180.0);
+            ctx.beginPath();
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(390, 360);
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.moveTo(x2, y2);
+            
+            ctx.arc(x2, y2, 10, 0, 2 * Math.PI);
+            ctx.fillStyle = "#FF0000";
+            ctx.moveTo(x1,y1);
+            ctx.arc(x1, y1, 10, 0, 2 * Math.PI);
+            ctx.fill();
 
-        ctx.font = "20px Arial";
-        ctx.fillText("Néző", 20, 390);
-        ctx.font = "20px Arial";
-        ctx.fillText("Égitest", x2 - 20, y2 - 20);
-        ctx.stroke();
+            ctx.font = "20px Arial";
+            ctx.fillText("Néző", 20, 390);
+            ctx.font = "20px Arial";
+            ctx.fillText("Égitest", x2 - 20, y2 - 20);
+            ctx.stroke();
+        }else{
+            let Declination1 = object[0].declination.split(/(\s)/);
+            let /*longitude*/ RightAscension1= object[0].right_ascension.split(" ");
+            var Declination2 = [];
+            var RightAscension2 = [];
+            for (let i = 0; i < RightAscension1.length; i++) {
+            RightAscension2[i] = Number(RightAscension1[i].slice(0,RightAscension1[i].length -1));
+            }       
+            var longitudeStar = RightAscension2[0] + RightAscension2[1]/60 + RightAscension2[2]/3600;
+            if (longitudeStar > 180) {
+                longitudeStar -= 360;
+            }
+            for (let i = 0; i < 3; i++) {
+                Declination2[i] = Number(Declination1[(i*2)].slice(0,Declination1[(i*2)].length -1));
+                }
+            var latitudeStar = Declination2[0] + Declination2[1]/60 + Declination2[2]/3600;
+            var longitudePerson= document.getElementById("longitude").value;
+            var latitudePerson= document.getElementById("latitude").value;
+            var latitudeDifference = latitudeStar - latitudePerson;
+            var longitudeDifference = longitudeStar - longitudePerson;
+            latitudeDifference = latitudeDifference * Math.PI/180;
+            longitudeDifference = longitudeDifference * Math.PI/180;
+            var semidistance = Math.pow(Math.sin(latitudeDifference/2),2)+Math.cos(latitudePerson)*Math.cos(latitudeStar)*Math.pow(Math.sin(longitudeDifference/2),2);
+            var Distance = 2*6371*Math.pow(Math.atan2(Math.sqrt(semidistance),Math.sqrt(1-semidistance)),2);
+            var StarAzimuth = Math.atan2(Math.sin(longitudeDifference)*Math.cos(latitudeStar),Math.cos(latitudePerson)*Math.sin(latitudeStar)-Math.sin(latitudePerson)*Math.cos(latitudeStar)*Math.cos(longitudeDifference));
+            StarAzimuth = StarAzimuth * 180/Math.PI;
+        }
     }
 
     function clear() {
